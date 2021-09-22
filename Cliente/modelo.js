855de353c6fa2a7ed96ba6b8fefd367e3304320e
Alfonm35 <alfonsoiniesta35@hotmail.es>
2021-09-22 16:22:36 +0200

    Añadido codigo en modelo.js

--- Cliente/modelo.js
+++ Cliente/modelo.js
@@ -1,16 +1,27 @@
 function Juego(){
-    this.jugadores={};
+    this.jugadores={}; //Array asociativo - Nick como campo único
+    this.partidas={};
 
     this.agregarJugador=function(nick){
         if(!this.jugadores[nick]){
-        var jugador = new Jugador(nick);
+        var jugador = new Jugador(nick,this);
         this.jugadores[nick]=jugador;
         }
     }
+
+    this.crearPartida=function(nick,numJug){
+        //crear código único
+        //crear la instancia de la Partida
+        //asignarla a la colección de partidas
+    }
 }
 
-function Jugador(nick){
+function Jugador(nick,juego){
     this.nick=nick;
+    this.juego=juego;
+    this.crearPartida=function(numJug){
+        this.juego.crearPartida(nick, numJug)
+    }
 }
 
 function Partida(nombre){
